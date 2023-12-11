378911f1fddb99f1a9823e4f71e65f2e3901cb5a
jiangjunlin1234 <1973055591@qq.com>
2023-12-11 21:38:00 +0800

    2023年12月11日21:37:24 第一次提交

---
+++ rabbitmq-plus-system/src/main/java/org/code/system/RabbitmqPlusSystemApplication.java
@@ -0,0 +1,13 @@
+package org.code.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqPlusSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqPlusSystemApplication.class, args);
+	}
+
+}
---
+++ rabbitmq-plus-system/src/test/java/org/code/system/RabbitmqPlusSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.code.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqPlusSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
